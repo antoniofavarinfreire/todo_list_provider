5a9d0620da893c370f457438682fb0c56a782d7a
Antonio Favarin Freire <favarinfreire@hotmail.com>
2023-06-22 16:57:21 -0300

    Criando o front da tela de menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/app_module.dart';
 
 void main() {
-  runApp(Container(
-    color: Colors.red,
-  ));
+  runApp(AppModule());
 }
